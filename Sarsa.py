d78582c889aed89e0fbdfbd0be24a1467f781894
topspinjake <topspinjake@gmail.com>
2019-01-07 18:20:59 -0800

    Begin Reinforcement

--- Sarsa.py
+++ Sarsa.py
@@ -1,5 +1,18 @@
 # Jake Williams - program to train 21NN using on-policy Sarsa TD learning
 
 from Blackjack import *
+from NN import *
+import torch
+
+trainingEpochs = 100
+playEpochs = 100
+totalEpochs = 100
 
 env = Blackjack()
+
+#One play loop looks like:
+data = torch.zeros(14)
+while not env.done:
+    
+
+#One training loop looks like:
